79e7f91b7a8c14107a3a204cfd89cf39a28640e2
Ishan Kumar <ishuishankumar1@gmail.com>
2026-01-19 10:00:00 +0530

    feat: Add Google Authentication setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      // Add user ID to the session
+      if (session.user) {
+        // @ts-expect-error adding id
+        session.user.id = token.sub;
+      }
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
